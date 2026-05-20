52492a5fafa16853e7ff93be1b39ca86b123568c
thanvithakariveda <thanvithakariveda06@gmail.com>
2026-05-20 19:47:17 +0530

    fix route import

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
