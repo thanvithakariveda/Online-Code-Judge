baaeca2baac3bbc25a4979c3a4390ea48e0dd01e
thanvithakariveda <thanvithakariveda06@gmail.com>
2026-05-21 00:12:25 +0530

    fix routing issues

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "../App.jsx";
 import Home from "../pages/Home.jsx";
 import Login from "../pages/Login.jsx";
 import Register from "../pages/Register.jsx";
