2589eebc86c27f87916c920b3b5129cc05867fb9
thanvithakariveda <thanvithakariveda06@gmail.com>
2026-05-21 01:38:14 +0530

    fix authRoutes import path

--- backend/routes/submission.routes.js
+++ backend/routes/submission.routes.js
@@ -1,10 +1,9 @@
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { getMySubmissions } from "../controllers/submissionController.js";
+import { getMySubmissions } from "../controllers/submission.controller.js";
 
 const router = express.Router();
 
-// 🔥 MUST HAVE protect HERE
 router.get("/me", protect, getMySubmissions);
 
 export default router;
